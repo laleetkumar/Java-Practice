d5d4436fdc20dcd22e1e99e8c3ecebc188457d2a
Laleet Kumar <laleetkumar275@gmail.com>
2023-08-19 19:06:01 +0500

    Even Number Program

---
+++ EvenNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class EvenNumber{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		
+		int i = 1 ;
+		while( i <= 10){
+					
+			
+			i++;
+			if(i % 2 == 0)
+			{
+				System .out.println(i);
+			}
+		}
+		
+		}
+		}
